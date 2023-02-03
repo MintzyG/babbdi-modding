1dcf03efc3ddff4bbef35cd1991b47ff633b8189
Vasilis The Pikachu <vasilis@pikachu.systems>
2023-02-03 23:48:14 +0100

    Setup

---
+++ debabbdi/main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace debabbdi
+{
+    public class debabbdi : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Debabbdi has been loaded!");
+        }
+    }
+}
